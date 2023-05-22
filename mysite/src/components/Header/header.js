98dd1845d02a47abdf76c26bae1beba2157d28c5
Daniel Tan <tsdaniel0927@gmail.com>
2023-05-22 22:54:54 +0800

    create header component

---
+++ mysite/src/components/Header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function myHeader(){
+    return(
+        <div>
+            <a>CV</a>
+            <a>Projects</a>
+        </div>
+    )
+}
+
+export default myHeader
