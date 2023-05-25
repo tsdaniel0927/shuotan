31503c245591559a0163874456708a61d64d6ea4
Daniel Tan <tsdaniel0927@gmail.com>
2023-05-25 23:33:50 +0800

    Top NavBar

--- mysite/src/components/Header/header.js
+++ mysite/src/components/Header/header.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import {AppBar, Box, Toolbar, Typography, CssBaseline} from '@mui/material'
 
 function Myheader(){
     return(
-        <div>
-            <a>CV</a>
-            <a>Projects</a>
-        </div>
+        <React.Fragment>
+            <CssBaseline />
+            <Box sx={{flexGrow: 1}}>
+                <AppBar position='static' s>  
+                    <Toolbar>
+                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                            Shuo Tan
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+            <div>
+                <header>Hi</header>
+            </div>
+            </Box>
+        </React.Fragment>
     )
 }
 
