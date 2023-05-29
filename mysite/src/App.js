3f203443e06bb4cade8e7dfe9f65f8b439d26ca7
Daniel Tan <tsdaniel0927@gmail.com>
2023-05-30 00:41:36 +0800

    finished styling of footer and top navbar layout

--- mysite/src/App.js
+++ mysite/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Myfooter from './components/Footer/footer';
 import Myheader from './components/Header/navbar';
+import './index.css'
+// import { useRef } from 'react';      check for the window's height
 
 function App() {
+  // const windowSize = useRef([window.innerWidth, window.innerHeight])
   return (
     <div>
       <Myheader/>
+        <div className='flex-wrapper'>This is a short content</div>
+        {/* <div>Height: {windowSize.current[1]}</div> */}
+      <Myfooter/>
     </div>
   );
 }
