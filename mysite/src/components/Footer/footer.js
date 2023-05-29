47db0341045189d62b28f9b3df7e0594d8c937ca
Daniel Tan <tsdaniel0927@gmail.com>
2023-05-30 00:45:59 +0800

    footer design

---
+++ mysite/src/components/Footer/footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './footer.css'
+
+function Myfooter(){
+    return(
+        <div className="footer"><p>footer here</p></div>
+    )
+}
+
+export default Myfooter
