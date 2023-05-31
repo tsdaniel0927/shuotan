2d4159a2ac98f29a8b268c106aacdb404c469110
Daniel Tan <tsdaniel0927@gmail.com>
2023-05-31 19:51:34 +0800

    footer style refined

--- mysite/src/components/Footer/footer.js
+++ mysite/src/components/Footer/footer.js
@@ -1,14 +1,18 @@
 import React from "react";
 import './footer.css'
 import LinkedInIcon from '@mui/icons-material/LinkedIn'
-import GitHubIcon from '@mui/icons-material/GitHub'; 
+import GitHubIcon from '@mui/icons-material/GitHub'
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 function Myfooter(){
     return(
         <div className="footer">
-            {/* <p>footer here</p> */}
-            <a><LinkedInIcon/></a>
-            <a><GitHubIcon/></a>
+            <div className="footer-wrapper">
+                <a style={{fontSize: "1rem", marginTop: "10px", marginRight:"110vh"}}>© 2023. All Rights Reserved.</a>
+                <a href="https://www.linkedin.com/in/daniel-tan-37870a133" className="icons"><LinkedInIcon/></a>
+                <a href="https://github.com/tsdaniel0927" className="icons"><GitHubIcon/></a>
+                <a href="https://www.instagram.com/svker_4_pain/" className="icons"><InstagramIcon/></a>
+            </div>
         </div>
     )
 }
