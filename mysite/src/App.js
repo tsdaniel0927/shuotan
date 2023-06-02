760894eb65f44ab274e43e364414f0b57f8e6413
Daniel Tan <tsdaniel0927@gmail.com>
2023-06-02 20:54:16 +0800

    update the main page, timeout function still need to be added

--- mysite/src/App.js
+++ mysite/src/App.js
@@ -1,15 +1,52 @@
+import React from 'react';
+import {useReward} from 'react-rewards'
+import { TextField, Button, Collapse } from '@mui/material';
 import './App.css';
 import Myfooter from './components/Footer/footer';
 import Myheader from './components/Header/navbar';
-// import { useRef } from 'react';      check for the window's height
+
 
 function App() {
-  // const windowSize = useRef([window.innerWidth, window.innerHeight])
+  const [name, setName] = React.useState('')
+  const [showcontent, setShowcontent] = React.useState(false)
+  const [showquestion, setShowquestion] = React.useState(true)
+  const {reward, isAnimating} = useReward('rewardID', 'confetti')
+
+  function handleChange(e){
+    setName(e.target.value)
+  }
+
+  
+
+  function handleClick(){
+    if(name === 'Shuo Tan'){
+      reward()
+
+      setShowcontent(true)
+      setShowquestion(false)
+    }else{
+      window.alert('Nope, wrong anwser 🥲, try again')
+    }
+  }
+
   return (
     <div>
       <Myheader/>
-        <div className='flex-wrapper'>This is a short content</div>
-        {/* <div>Height: {windowSize.current[1]}</div> */}
+      <div className='flex-wrapper'>
+        <div style={{  color: "grey"}}>
+          <Collapse in={showquestion}>
+            what's my name again? &emsp;&emsp; 
+            <TextField variant='standard' size='small' value={name} onChange={handleChange}></TextField>
+            <Button id='rewardID' onClick={handleClick} disabled={isAnimating}> This is it!</Button>
+          </Collapse>
+          {showcontent && (
+            <Collapse in={showcontent}>
+              <p className='sub-heading'>About</p>
+            </Collapse>
+          )}
+          
+        </div>
+      </div>
       <Myfooter/>
     </div>
   );
