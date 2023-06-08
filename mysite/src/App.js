7b2f4872150456e5b1782e33ab25d51b90f976cc
Daniel Tan <tsdaniel0927@gmail.com>
2023-06-08 20:50:03 +0800

    added router

--- mysite/src/App.js
+++ mysite/src/App.js
@@ -1,68 +1,31 @@
-import React, {useEffect} from 'react';
-import {useReward} from 'react-rewards';
-import { TextField, Button, Collapse } from '@mui/material';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import About from './pages/About';
 import Myfooter from './components/Footer/footer';
 import Myheader from './components/Header/navbar';
-
+import Verify from './pages/Verification';
+import PrivateRoute from './pages/PrivateRoute';
+import Home from './pages/Home';
+import Error from './pages/Error';
 
 function App() {
-  const [name, setName] = React.useState('')
-  const [showcontent, setShowcontent] = React.useState(false)
-  const [showquestion, setShowquestion] = React.useState(true)
-  const {reward, isAnimating} = useReward('rewardID', 'confetti')
+  // const isVerified = localStorage.getItem === 'Shuo Tan';
 
-  // check this one later, when page routing done
-  useEffect(() => {
-    const storedName = localStorage.getItem('userAnswer');
-    if (storedName === 'Shuo Tan') {
-      console.log('local storage has been accessed')
-      setShowcontent(true);
-      setShowquestion(false);
-    }
-  }, []);
-
-  function handleChange(e){
-    setName(e.target.value)
-  }
-
-  function handleClick(){
-    if(name === 'Shuo Tan'){
-      reward()
-      setTimeout(() => {setShowcontent(true);setShowquestion(false)}, 2000);
-    }else{
-      window.alert('Nope, wrong anwser 🥲, try again')
-    }
-  }
-
+  
   return (
-    <div>
-      <Myheader/>
-      <div className='flex-wrapper'>
-        <div className='container'>
-          <div style={{
-            marginTop: showcontent ? '0' : '100px',
-            textAlign: showcontent ? 'center':''
-          }}>
-            <Collapse in={showquestion}>
-              What's my name again? &emsp;&emsp; 
-              <TextField variant='standard' size='small' value={name} onChange={handleChange}></TextField>
-              <Button id='rewardID' onClick={handleClick} disabled={isAnimating}> This should be it!</Button>
-            </Collapse>        
-          </div>
-        </div>
-        <div style={{
-            marginTop: showcontent ? '0' : ''
-          }}>
-            {showcontent && (
-              <Collapse in={showcontent}>
-                <p className='sub-heading'>About</p>
-              </Collapse>
-            )} 
-          </div>
+    <Router>
+      <div>
+        <Myheader />
+        <Routes>
+          <Route path="/" element={<Verify />} />
+          <Route path="/about" element={<PrivateRoute><About /></PrivateRoute>} />
+          <Route path="/home" element={<PrivateRoute><Home /></PrivateRoute>} />
+          <Route path="/error" element={<Error />} />
+        </Routes>
+        <Myfooter />
       </div>
-      <Myfooter/>
-    </div>
+    </Router>
   );
 }
 
