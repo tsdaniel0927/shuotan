5db7245f6a804e40b6bdb20214be6864cc2bb864
Daniel Tan <tsdaniel0927@gmail.com>
2023-06-08 20:48:08 +0800

    added style of  phone and tablet view of navbar

--- mysite/src/components/Header/navbar.js
+++ mysite/src/components/Header/navbar.js
@@ -1,30 +1,41 @@
 import React from 'react';
-import {AppBar, Box, Toolbar, Typography, CssBaseline, Button} from '@mui/material'
+import { AppBar, Box, Toolbar, Typography, CssBaseline, Button } from '@mui/material';
+import { Link } from 'react-router-dom';
 
-function Myheader(){
-    const style = {
-        color: 'white',
-        fontWeight: 900,
-        fontSize: 17,
-    }
-    return(
-        <React.Fragment>
-            <CssBaseline />
-            <Box sx={{flexGrow: 1}}>
-                <AppBar position='static' style={{backgroundColor: '#366091'}}>  
-                    <Toolbar>
-                        <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Pacifico'/>
-                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} style={{fontFamily: 'Pacifico', color: 'white'}}>
-                            Shuo Tan
-                        </Typography>
-                        <Button style={style}>About</Button>
-                        <Button style={style}>Projects</Button>
-                        <Button style={style}>ME</Button>
-                    </Toolbar>
-                </AppBar>
-            </Box>
-        </React.Fragment>
-    )
+function Myheader() {
+  const style = {
+    color: 'white',
+    fontWeight: 900,
+  };
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" style={{ backgroundColor: '#366091' }}>
+          <Toolbar>
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Pacifico" />
+            <Typography variant="h8" component="div" sx={{ flexGrow: 1 }} style={{ fontFamily: 'Pacifico', color: 'white' }}>
+              <Link className='home-link' style={{ textDecoration: 'none', color: 'white' }} to="/">
+                Shuo Tan
+              </Link>
+            </Typography>
+            <Link to="/about">
+              <Button size='small' sx={{ ...style, '@media (max-width: 600px)': { fontSize: 12 }, '@media (min-width: 601px) and (max-width: 960px)': { fontSize: 14 } }}>
+                About
+              </Button>
+            </Link>
+            <Button  size='small' sx={{ ...style, '@media (max-width: 600px)': { fontSize: 12 }, '@media (min-width: 601px) and (max-width: 960px)': { fontSize: 14 } }}>
+              Projects
+            </Button>
+            <Button  size='small' sx={{ ...style, '@media (max-width: 600px)': { fontSize: 12 }, '@media (min-width: 601px) and (max-width: 960px)': { fontSize: 14 } }}>
+              ME
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </React.Fragment>
+  );
 }
 
-export default Myheader
+export default Myheader;
