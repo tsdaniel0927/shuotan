961bc4048dd36bc8d4d294f4bdc6e36bee305a93
Daniel Tan <tsdaniel0927@gmail.com>
2023-06-24 01:26:28 +0800

    blocked footer view on error and verification page

--- mysite/src/App.js
+++ mysite/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import About from './pages/About';
 import Myfooter from './components/Footer/footer';
 import Myheader from './components/Header/navbar';
@@ -8,26 +8,35 @@ import PrivateRoute from './pages/PrivateRoute';
 import Home from './pages/Home';
 import Error from './pages/Error';
 import OHCWA from './pages/Projects/OHCWA';
-import FaceDetection from './pages/Projects/FaceDetection'
+import FaceDetection from './pages/Projects/FaceDetection';
+import Certifications from './pages/Certifications';
 
-function App() {
-  // const isVerified = localStorage.getItem === 'Shuo Tan';
-
+function AppRoutes() {
+  const location = useLocation();
+  const isErrorPage = location.pathname === '/error';
+  const isVerified = location.pathname === '/'
 
   return (
+    <div>
+      <Myheader />
+      <Routes>
+        <Route path="/" element={<Verify />} />
+        <Route path="/about" element={<PrivateRoute><About /></PrivateRoute>} />
+        <Route path="/home" element={<PrivateRoute><Home /></PrivateRoute>} />
+        <Route path="/projects/ohcwa" element={<PrivateRoute><OHCWA /></PrivateRoute>} />
+        <Route path="/projects/facedetection" element={<PrivateRoute><FaceDetection /></PrivateRoute>} />
+        <Route path="/certifications" element={<PrivateRoute><Certifications /></PrivateRoute>} />
+        <Route path="/error" element={<Error />} />
+      </Routes>
+      {!isErrorPage && !isVerified && <Myfooter />}
+    </div>
+  );
+}
+
+function App() {
+  return (
     <Router>
-      <div>
-        <Myheader />
-        <Routes>
-          <Route path="/" element={<Verify />} />
-          <Route path="/about" element={<PrivateRoute><About /></PrivateRoute>} />
-          <Route path="/home" element={<PrivateRoute><Home /></PrivateRoute>} />
-          <Route path="/projects/ohcwa" element={<PrivateRoute><OHCWA /></PrivateRoute>} />
-          <Route path="/projects/facedetection" element={<PrivateRoute><FaceDetection /></PrivateRoute>} />
-          <Route path="/error" element={<Error />} />
-        </Routes>
-        <Myfooter />
-      </div>
+      <AppRoutes />
     </Router>
   );
 }
