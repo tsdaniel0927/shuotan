adefe419bffc080cb1b7d0bebc9c6d4fe6b7791e
Daniel Tan <tsdaniel0927@gmail.com>
2023-08-24 22:58:24 +0800

    added more certifications and changed a bit styling

--- mysite/src/pages/Certifications.js
+++ mysite/src/pages/Certifications.js
@@ -2,6 +2,8 @@ import React from "react";
 import './certifications.css'
 import MasterTestaumur from '../images/certifications/MasterTestamur.jpg'
 import BachelorTestamur from '../images/certifications/BachelorTestamur.jpg'
+import OOP from '../images/certifications/OOP.jpg'
+import SDLC from '../images/certifications/SDLC.jpg'
 import { Fade } from "@mui/material";
 
 export default function Home(){
@@ -11,11 +13,13 @@ export default function Home(){
                 <div className="wrapper-left-certifications">
                     <span className="sub-heading"> Degree Certificate </span>
                     <img className="certification-image" src={MasterTestaumur} alt="certificationMaster"/>
-                    <span className="sub-heading" style={{marginTop:'4.5vw', marginBottom:'20px'}}> More certifications ongoing... </span>
+                    <img className="certification-image" src={OOP} alt="certificationOOP"/>
+                    <span className="sub-heading" style={{marginTop:'2vw', marginBottom:'20px'}}> More certifications ongoing... </span>
                 </div>
                 
                 <div className="wrapper-right-certifications" style={{marginTop:'27px'}}>
                     <img className="certification-image" src={BachelorTestamur} alt="certificationBachelor"/>
+                    <img className="certification-image" src={SDLC} alt="certificationSDLC"/>
                 </div>
             </div>
         </Fade>
